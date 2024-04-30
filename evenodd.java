0dfaab577f5080d13f52d39c8b4169300ffc9220
purunaveen <167996708+purunaveen@users.noreply.github.com>
2024-04-30 10:13:49 +0530

    Add files via upload

---
+++ evenodd.java
@@ -0,0 +1,32 @@
+package evenOddaInt;
+
+public class evenodd {
+
+	public static void main(String[] args) {
+		
+
+		int a[]={1,3,5,2,4,6,11,12,15,16};  
+		System.out.println("Odd Numbers:");  
+		for(int i=0;i<a.length;i++){  
+		if(a[i]%2!=0){  
+		System.out.println(a[i]);  
+		}  
+		}  
+		System.out.println("Even Numbers:");  
+		for(int i=0;i<a.length;i++){  
+		if(a[i]%2==0){  
+		System.out.println(a[i]);  
+		}  
+		}  
+	}
+					
+
+		}
+
+		
+
+	
+
+	
+
+
